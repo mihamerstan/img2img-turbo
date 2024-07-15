5e6c78886755b34be3891bbf420194ff94e13f2f
Michael <michael.hamer.stanley@gmail.com>
2024-07-14 20:04:21 -0700

    allow for inference on image_folder in inference_unpaired.py

--- src/inference_unpaired.py
+++ src/inference_unpaired.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import argparse
 from PIL import Image
 import torch
@@ -7,9 +8,28 @@ from cyclegan_turbo import CycleGAN_Turbo
 from my_utils.training_utils import build_transform
 
 
+def translate_image(args, img_path, model, T_val):
+        input_image = Image.open(img_path).convert('RGB')
+        # translate the image
+        with torch.no_grad():
+            input_img = T_val(input_image)
+            x_t = transforms.ToTensor()(input_img)
+            x_t = transforms.Normalize([0.5], [0.5])(x_t).unsqueeze(0).cuda()
+            output = model(x_t, direction=args.direction, caption=args.prompt)
+
+        output_pil = transforms.ToPILImage()(output[0].cpu() * 0.5 + 0.5)
+        output_pil = output_pil.resize((input_image.width, input_image.height), Image.LANCZOS)
+
+        # save the output image
+        bname = os.path.basename(img_path)
+        os.makedirs(args.output_dir, exist_ok=True)
+        output_pil.save(os.path.join(args.output_dir, bname))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_image', type=str, required=True, help='path to the input image')
+    parser.add_argument('--input_image', type=str, default=None, help='path to the input image')
+    parser.add_argument('--input_folder', type=str, default=None, help='path to the input images')
     parser.add_argument('--prompt', type=str, required=False, help='the prompt to be used. It is required when loading a custom model_path.')
     parser.add_argument('--model_name', type=str, default=None, help='name of the pretrained model to be used')
     parser.add_argument('--model_path', type=str, default=None, help='path to a local model state dict to be used')
@@ -36,19 +56,13 @@ if __name__ == "__main__":
     model.unet.enable_xformers_memory_efficient_attention()
 
     T_val = build_transform(args.image_prep)
-
-    input_image = Image.open(args.input_image).convert('RGB')
-    # translate the image
-    with torch.no_grad():
-        input_img = T_val(input_image)
-        x_t = transforms.ToTensor()(input_img)
-        x_t = transforms.Normalize([0.5], [0.5])(x_t).unsqueeze(0).cuda()
-        output = model(x_t, direction=args.direction, caption=args.prompt)
-
-    output_pil = transforms.ToPILImage()(output[0].cpu() * 0.5 + 0.5)
-    output_pil = output_pil.resize((input_image.width, input_image.height), Image.LANCZOS)
-
-    # save the output image
-    bname = os.path.basename(args.input_image)
-    os.makedirs(args.output_dir, exist_ok=True)
-    output_pil.save(os.path.join(args.output_dir, bname))
+    
+    if args.input_image is not None:
+        translate_image(args, args.input_image, model, T_val)
+    if args.input_folder is not None:
+        img_paths = glob.glob(args.input_folder+"/*.jpg")
+        num_images = len(img_paths)
+        print("num images: ", num_images)
+        for i, img_path in enumerate(img_paths):
+            translate_image(args, img_path, model, T_val)
+            print(f"Image {img_path} completed ({i}/{num_images}).")
